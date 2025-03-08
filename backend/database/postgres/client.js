e890115573cae7031443de18c31cfd3a7bef8ff1
Amit <amitkumarmandal2111@gmail.com>
2025-03-08 13:36:36 +0530

    (feat): added correct username

--- backend/database/postgres/client.js
+++ backend/database/postgres/client.js
@@ -8,9 +8,16 @@ export const getPool = () => pool;
 export const initalizeDatabase = async () => {
   if (pool) return;
   try {
+    console.log({
+      host: process.env.HOST, // Ensure the correct env variable
+      user: process.env.DATABASE_USER||"postgres", // Correct the 'username' to 'user'
+      database: process.env.DATABASE, // Correct the 'database' reference
+      password: process.env.PASSWORD, // Don't forget password
+      port: 5432, // Default to 5432 if no port is set
+    })
     pool = new Pool({
       host: process.env.HOST, // Ensure the correct env variable
-      user: process.env.USER, // Correct the 'username' to 'user'
+      user: process.env.DATABASE_USER, // Correct the 'username' to 'user'
       database: process.env.DATABASE, // Correct the 'database' reference
       password: process.env.PASSWORD, // Don't forget password
       port: 5432, // Default to 5432 if no port is set
