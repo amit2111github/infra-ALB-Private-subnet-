20b4487cc649546f9947b33b7d09d12fa2114315
Amit <amitkumarmandal2111@gmail.com>
2025-03-08 14:34:32 +0530

    (feat): ip in test route

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,30 @@ import dotenv from "dotenv";
 import { initalizeDatabase } from "./database/postgres/client.js";
 import cors from "cors";
 import bodyParser from "body-parser";
+import os from "os"
 import { migration } from "./migration.js";
 
 dotenv.config();
 await initalizeDatabase()
 await migration();
-
+function getIp() {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+        const networkInterface = networkInterfaces[interfaceName];
+        for (const address of networkInterface) {
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address
+            }
+        }
+    }
+    return "could not find ip";
+}
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.get("/test" , (req , res) => {
-    res.status(200).json({status : "ok"});
+    res.status(200).json({status : "ok" , ip : getIp()});
 })
 
 app.use("/user" , userRouter);
